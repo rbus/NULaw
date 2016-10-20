ee4ad9f0d9b804ce8ba738bff44c6dc5f0e85546
Rbus Ugrit <Rbus Ugrit>
2016-10-20 15:35:15 +0700

    NU Law

---
+++ js/config.js
@@ -0,0 +1,81 @@
+$(function() {
+
+  CMS.init({
+
+    // Name of your site or location of logo file, relative to root directory (img/logo.png)
+    siteName: 'RBUS',
+
+    // Tagline for your site
+    siteTagline: 'site tagline',
+
+    // Email address
+    siteEmail: 'rbus.sk@gmail.com',
+
+    // Name
+    siteAuthor: 'RBUS',
+
+    // Navigation items
+    siteNavItems: [
+      { name: 'Github', href: 'https://github.com/rbus', newWindow: false},
+      { name: 'About'}
+    ],
+
+    // Posts folder name
+    postsFolder: 'posts',
+
+    // Homepage posts snippet length
+    postSnippetLength: 120,
+
+    // Pages folder name
+    pagesFolder: 'pages',
+
+    // Order of sorting (true for newest to oldest)
+    sortDateOrder: true,
+
+    // Posts on Frontpage (blog style)
+    postsOnFrontpage: true,
+
+    // Page as Frontpage (static)
+    pageAsFrontpage: '',
+
+    // Posts/Blog on different URL
+    postsOnUrl: '',
+
+    // Site fade speed
+    fadeSpeed: 300,
+
+    // Site footer text
+    footerText: '&copy; ' + new Date().getFullYear() + ' All Rights Reserved.',
+
+    // Mode 'Github' for Github Pages, 'Server' for Self Hosted. Defaults
+    // to Github
+    mode: 'Github',
+
+     // If Github mode is set, your Github username and repo name.
+    githubUserSettings: {
+      username: 'rbus',
+      repo: 'NULaw'
+    },
+
+    // If Github mode is set, choose which Github branch to get files from.
+    // Defaults to Github pages branch (gh-pages)
+    githubSettings: {
+      branch: 'gh-pages',
+      host: 'https://api.github.com'
+    }
+
+  });
+
+  // Markdown settings
+  marked.setOptions({
+    renderer: new marked.Renderer(),
+    gfm: true,
+    tables: true,
+    breaks: false,
+    pedantic: false,
+    sanitize: true,
+    smartLists: true,
+    smartypants: false
+  });
+
+});
